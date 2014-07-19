5fc3c7d6aca87a63e355522a643968cc52cbc559
Suman Paul <sumank.paul@gmail.com>
2014-07-19 08:41:54 +0530

    comit

--- controller/controller.js
+++ controller/controller.js
@@ -1,18 +1,24 @@
 //controller
 angular.module('app', [])
   .controller('Ctrl', function ($scope) {
-
+    $scope.callFromCtrl = function () {
+      console.log('controller method called');
+    };
   })
   .directive('dirZero', function () {
     return {
       restrict: 'EA',
       controller: function ($scope, $element, $attrs, $log) {
         console.log('inside controller');
-        console.log($scope, $element, $attrs);
         $log.log('using injected service');
       },
-      link: function () {
+      link: function (scope, elem, attr) {
         console.log('in link');
+        elem.bind("mouseenter", function () {
+          console.log('in apply');
+          scope.$apply('callFromCtrl()');
+//          scope.$apply(attr.callMethod);
+        })
       }
     }
   })
